db21697ade8b25cc849603fd3c184d451259cd5d
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2017-11-03 20:43:29 -0600

    Nuevas paginas

---
+++ MEI-Master/MEI/recomendaciones.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Pagina principal | MEI</title>
+    </head>
+    <body>
+        <?php include '../resourses/header.html'; ?>
+        <?php include '../resourses/footer.html'; ?>
+    </body>
+</html>
