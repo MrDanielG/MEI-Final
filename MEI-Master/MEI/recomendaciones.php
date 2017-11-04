1b1c3f4eb5ded35871a60dfc361df3f13a8aa3fd
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2017-11-03 21:07:55 -0600

    Cambios en las paginas nuevas

--- MEI-Master/MEI/recomendaciones.php
+++ MEI-Master/MEI/recomendaciones.php
@@ -1,9 +1,11 @@
 <html>
     <head>
         <title>Pagina principal | MEI</title>
+        <?php include '../resourses/header.html'; ?>
     </head>
     <body>
-        <?php include '../resourses/header.html'; ?>
+        <?php include '../resourses/menu.html'; ?>
         <?php include '../resourses/footer.html'; ?>
+        <?php include '../resourses/scripts.html'; ?>
     </body>
 </html>
